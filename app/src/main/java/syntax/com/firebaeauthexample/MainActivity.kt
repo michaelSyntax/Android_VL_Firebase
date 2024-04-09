eafe24760c8851c828bcb2137c35f727c48985f0
michaelSyntax <michael.karbe@syntax-institut.de>
2024-04-09 04:21:14 +0200

    Initial commit

---
+++ app/src/main/java/syntax/com/firebaeauthexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package syntax.com.firebaeauthexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
